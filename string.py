ac43fd5864faaaa03899eafb7f4caa2573f7ba1b
Umesh Rana <ur621799@gmail.com>
2021-04-28 08:09:22 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+# a = 'abcdefghij'
+# def abc(s):
+#     return s[7]
+# print(abc(a))
+
+
+# a = 'a b c d e f g h i j'
+# def abc(s):
+#
+#     #return s.split()
+#     return s[0:4]
+# print(abc(a))
+
+def xyz(l):
+
+   return sorted(l)
+abc = [3,1,4,2,8,4]
+print(xyz(abc))
